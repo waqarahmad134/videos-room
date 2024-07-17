7d711642949c825af1648455456ce6607e5e812c
Waqar Ahmad <47711874+waqarahmad134@users.noreply.github.com>
2024-07-17 17:53:23 +0500

    Update URL.js

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -1,4 +1,5 @@
-export const imgURL = "https://movie.theviralmind.com/thumbnail/";
+// export const imgURL = "https://movie.theviralmind.com/thumbnail/";
+export const imgURL = "https://movie.theviralmind.com/public/thumbnail/";
 export const DOMIAN_URL = "https://movie.theviralmind.com/public/";
 export const BASE_URL = "https://movie.theviralmind.com/public/api/";
 // export const DOMIAN_URL = "http://localhost:8000/";
