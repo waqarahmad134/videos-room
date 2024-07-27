803cbaac971449353ee5449fe6b351b59b1adc70
Waqar Ahmad <waqar@quoli.io>
2024-07-28 03:46:06 +0500

    lazy loading added for speed optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import About from "./pages/About";
-import Post from "./pages/Post";
+import React, { Suspense, lazy } from "react";
 import Home from "./pages/Home";
 import ErrorPage from "./errors/error-page";
 import { ToastContainer } from "react-toastify";
 import { ChakraProvider } from "@chakra-ui/react";
-import Genre from "./pages/Genre";
-import Year from "./pages/Year";
-import Search from "./pages/Search";
 import { HelmetProvider } from "react-helmet-async";
-import RequestMovie from "./pages/RequestMovie";
-import { DataProvider } from "./context/DataContext"; // Import the DataProvider
+import { DataProvider } from "./context/DataContext";
+
+// Lazy load the other components
+const About = lazy(() => import("./pages/About"));
+const Post = lazy(() => import("./pages/Post"));
+const Genre = lazy(() => import("./pages/Genre"));
+const Year = lazy(() => import("./pages/Year"));
+const Search = lazy(() => import("./pages/Search"));
+const RequestMovie = lazy(() => import("./pages/RequestMovie"));
 
 function App() {
   return (
@@ -22,12 +25,54 @@ function App() {
             <DataProvider>
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/request-movie" element={<RequestMovie />} />
-                <Route path="/movie/:slug" element={<Post />} />
-                <Route path="/:categories/:Id" element={<Genre />} />
-                <Route path="/year/:year" element={<Year />} />
+                <Route
+                  path="/about"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <About />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/search"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <Search />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/request-movie"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <RequestMovie />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/movie/:slug"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <Post />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/:categories/:Id"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <Genre />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/year/:year"
+                  element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <Year />
+                    </Suspense>
+                  }
+                />
                 <Route path="*" element={<ErrorPage />} />
               </Routes>
             </DataProvider>
